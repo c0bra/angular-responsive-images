9f5bd7597b4e0e1fce1cfe1fcfe9660b693e22cd
Brian Hann <emailc0bra@gmail.com>
2013-06-05 13:29:29 -0500

    WOP

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,15 +152,15 @@ module.exports = function(grunt) {
     promising(this,
       ensureCleanMaster().then(function () {
         shjs.rm('-rf', 'build');
-        return system('git checkout gh-pages');
+        return system('git', 'checkout gh-pages');
       }).then(function () {
-        return system('git merge master');
+        return system('git', 'merge master');
       }).then(function () {
-        return system('grunt build');
+        return system('grunt', 'build');
       }).then(function () {
         return system('git', 'commit -a -m \'Automatic gh-pages build\'');
       }).then(function () {
-        return system('git checkout master'); 
+        return system('git', 'checkout master'); 
       })
     );
   });
