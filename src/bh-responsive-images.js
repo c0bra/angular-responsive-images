29b07301ff7a058403d295c94c0d557027e83f7a
c0bra <emailc0bra@gmail.com>
2014-06-24 16:51:45 -0500

    Fixes #5: Remove listeners on destroy event

--- src/bh-responsive-images.js
+++ src/bh-responsive-images.js
@@ -137,6 +137,13 @@ app.directive('bhSrcResponsive', ['presetMediaQueries', '$timeout', function(pre
         //   updateFromQuery(querySets);
         // });
       });
+
+      // Remove media-query listeners when $scope is destroyed
+      scope.$destroy(function () {
+        angular.forEach(listenerSets, function(set) {
+          set.mql.removeListener(set.listener);
+        });
+      });
     }
   };
 }]);
